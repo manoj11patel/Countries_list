b7d9cf5f26a8a25e8fc26531bb16c6734b90463c
manoj11patel <mp.inviable@gmail.com>
2019-10-04 16:30:23 +0530

    countries list updated

--- countries.sql
+++ countries.sql
@@ -1,27 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Oct 04, 2019 at 04:04 PM
--- Server version: 5.7.27-0ubuntu0.16.04.1
--- PHP Version: 7.1.32-1+ubuntu16.04.1+deb.sury.org+1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `nine2five_api`
---
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `countries`
 --
@@ -309,6 +285,3 @@ ALTER TABLE `countries`
 --
 ALTER TABLE `countries`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=253;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
